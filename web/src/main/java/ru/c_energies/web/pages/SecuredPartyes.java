91201418aa0a39e4806d93768abd2095bf8bf1a4
bezrukikh.an <andreiiko2@rambler.ru>
2025-02-11 20:50:04 +0500

    1. Начало разработки обеспеченной стороны

---
+++ web/src/main/java/ru/c_energies/web/pages/SecuredPartyes.java
@@ -0,0 +1,23 @@
+package ru.c_energies.web.pages;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.c_energies.databases.Query;
+import ru.c_energies.databases.entity.appeals.AppealRow;
+import ru.c_energies.databases.sqlite.SqliteDataSource;
+import ru.c_energies.web.models.appeals.AppealsTable;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Controller
+public class SecuredPartyes {
+    @GetMapping(value = "/document/secured-party")
+    public String list(Model model) throws SQLException {
+        Query q = new Query(new SqliteDataSource(), "select * from appeals");
+        List<AppealRow> list = new AppealsTable(q.exec()).list();
+        model.addAttribute("list", list);
+        return "pages/secured-partyes";
+    }
+}
