ffde03c64629f677dd25627ad531c01cd8bb8d23
bezrukikh.an <andreiiko2@rambler.ru>
2025-04-02 18:27:11 +0500

    1. Подготовка вебсокетов

--- web/src/main/java/ru/c_energies/web/pages/Notifications.java
+++ web/src/main/java/ru/c_energies/web/pages/Notifications.java
@@ -1,12 +1,13 @@
 package ru.c_energies.web.pages;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import ru.c_energies.databases.entity.notifications.NotificationTable;
+import ru.c_energies.databases.entity.notifications.NotificationRow;
 import ru.c_energies.databases.entity.version.VersionTable;
 
 import java.sql.SQLException;
@@ -15,6 +16,9 @@ import java.util.Map;
 
 @Controller
 public class Notifications {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @GetMapping(value = "/notifications/version")
     public ResponseEntity<Object> version() throws SQLException {
         VersionTable versionTable = new VersionTable();
@@ -22,5 +26,12 @@ public class Notifications {
         Map<String, String> response = new HashMap<>();
         response.put("version", "1.0.0");
         return ResponseEntity.ok().body(response);
+    }
+
+    @MessageMapping("/notifications")
+    public void processMessage(@Payload NotificationRow notificationRow) {
+        messagingTemplate.convertAndSendToUser(
+                String.valueOf(notificationRow.id()),"/queue/messages",
+                notificationRow);
     }
 }
