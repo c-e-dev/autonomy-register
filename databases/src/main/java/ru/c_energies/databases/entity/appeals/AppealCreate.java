976b53a794c31468bc32c9276fe691361a08e357
bezrukikh.an <andreiiko2@rambler.ru>
2025-06-22 19:42:05 +0500

    Создание алгоритма по созданию внутреннего номера

--- databases/src/main/java/ru/c_energies/databases/entity/appeals/AppealCreate.java
+++ databases/src/main/java/ru/c_energies/databases/entity/appeals/AppealCreate.java
@@ -11,7 +11,7 @@ import java.time.Instant;
 public class AppealCreate implements Create<AppealCreate> {
     private final String INSERT = """
                 INSERT INTO appeals
-                (title, internal_number, register_track_number, create_date, due_date, answered, type)
+                (title, internal_number, register_track_number, create_date, due_date, answered, type, rule_internal_number)
                 VALUES('%s', '%s', '%s', %d, %d, %d, '%s', '%s') RETURNING rowid
             """;
     private final String INSERT_INTO_THEME = """
